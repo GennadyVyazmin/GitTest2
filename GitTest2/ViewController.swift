bfb0a1ec794a75f4c8bb9d71eb6ad38c753638bd
Геннадий Вязьмин <admin@zczn.ru>
2018-12-17 12:11:50 +0300

    Initial Commit

---
+++ GitTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest2
+//
+//  Created by Геннадий Вязьмин on 17/12/2018.
+//  Copyright © 2018 Геннадий Вязьмин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
